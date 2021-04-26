cb1005673f0ecb977bf959e8e76d990656b46514
TechDinith <80402982+TechDinith@users.noreply.github.com>
2021-04-26 12:13:55 +0530

    Add files via upload

---
+++ src/redux/actions.js
@@ -0,0 +1,31 @@
+export const ADD_TODO = "ADD_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+
+
+export function addTodo(todo) {
+    return {
+        type:ADD_TODO,
+        payload: todo,
+    }
+}
+
+export function deleteTodo(todoId) {
+    return {
+        type:DELETE_TODO,
+        payload: todoId,
+    }
+}
+
+
+export function updateTodo(todo) {
+    return {
+        type:UPDATE_TODO,
+        payload: todo,
+    }
+}
+
+export function completeTodo() {
+    document.getElementById("todo").style.textDecoration= "line-through";
+   
+}
